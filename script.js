3f2ad4ad1cb76421635617ab70a1ad81b47b6ae1
David <d.barcia9@gmail.com>
2022-08-03 14:36:43 +0200

    Micro Exercises 1

---
+++ script.js
@@ -0,0 +1,22 @@
+document.write("Hello World");
+
+document.write("<br>")
+    
+const num1 = 3;
+const num2 = 5 ;
+result = num1 + num2;
+
+
+document.write(result);
+document.write("<br>");
+
+var nombre = prompt("Escribe tu nombre");
+document.write("Hola "+nombre);
+document.write("<br>");
+
+var n1 = prompt("Escribe un número");
+var n2 = prompt("Escribe otro número");
+document.write("La suma es: "+ ( parseInt(n1)+ parseInt(n2) ) );
+
+    
+   
